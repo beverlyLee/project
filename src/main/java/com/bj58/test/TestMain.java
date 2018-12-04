330506709fb505905ba5e8826a51aeaf2bce743a
lby <bingjingshamo@163.com>
2018-12-04 11:23:06 +0800

    1.测试idea提交git

---
+++ src/main/java/com/bj58/test/TestMain.java
@@ -0,0 +1,17 @@
+package com.bj58.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        getDate();
+    }
+
+    private static void getDate(){
+        SimpleDateFormat sdf=new SimpleDateFormat("YYYY-MM-DD HH-mm-ss");
+        Date date=new Date();
+        String dateString=sdf.format(date);
+        System.out.println(dateString);
+    }
+}
